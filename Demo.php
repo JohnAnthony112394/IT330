6f02a59a62b7dd8c973e7b47367dd4278cde8390
JohnAnthony112394 <61747191+JohnAnthony112394@users.noreply.github.com>
2020-03-04 00:24:17 +0800

    Update Demo.php

--- Demo.php
+++ Demo.php
@@ -2,7 +2,30 @@
 	<body>
 	
 		<?php
-			echo "Hello World!";
+			$servername = "it330server1.mysql.database.azure.com";
+			$username = "JohnAnthony@it330server1";
+			$password = "Jo2hna3nthony";
+			$dbname = "mysampledb";
+
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			if ($conn->connect_error) {
+			    die("Connection failed: " . $conn->connect_error);
+			}
+
+			$sql = "SELECT id, name, count from inventory";
+			$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) {
+			    // output data of each row
+			    while($row = $result->fetch_assoc()) {
+				echo "id: " . $row["id"]. " - Product: " . $row["name"]. "  -Count:" . $row["count"]. "<br>";
+			    }
+			} else {
+			    echo "0 results";
+			}
+			$conn->close();
 		?>
 	
 	</body>
